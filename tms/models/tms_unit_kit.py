af25ef483a93190634edf9f9bd11cbc06e0a7617
Goran Kliska <goran.kliska@slobodni-programi.hr>
2017-07-12 10:34:12 +0200

    tms

---
+++ tms/models/tms_unit_kit.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012, Israel Cruz Argil, Argil Consulting
+# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import fields, models
+
+
+class TmsUnitKit(models.Model):
+    _name = "tms.unit.kit"
+    _inherit = ['mail.thread', 'ir.needaction_mixin']
+    _description = "Units Kits"
+
+    operating_unit_id = fields.Many2one(
+        'operating.unit', string='Operating Unit', required=True)
+    name = fields.Char(required=True)
+    unit_id = fields.Many2one('fleet.vehicle', 'Unit', required=True)
+    trailer1_id = fields.Many2one('fleet.vehicle', 'Trailer 1', required=True)
+    dolly_id = fields.Many2one('fleet.vehicle', 'Dolly')
+    trailer2_id = fields.Many2one('fleet.vehicle', 'Trailer 2')
+    employee_id = fields.Many2one(
+        'hr.employee', 'Driver', domain=[('driver', '=', True)])
+    date_start = fields.Datetime(required=True)
+    date_end = fields.Datetime(required=True)
+    notes = fields.Text()
+    active = fields.Boolean(default=(lambda *a: True))
+
+    _sql_constraints = [
+        ('name_uniq', 'unique(unit_id,name)',
+         'Kit name number must be unique for each unit !'),
+    ]
+    _order = "name desc, date_start desc"
